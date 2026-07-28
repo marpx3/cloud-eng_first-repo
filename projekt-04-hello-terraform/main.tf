7a0f3d1a49997cedd183d81f9d60b970f7febee9
Marco Podiwin <marco.podiwin+clouddev@gmail.com>
2026-07-28 14:15:51 +0200

    Projekt 4: Hello Terraform - deploy.sh deklarativ nachgebaut

--- projekt-04-hello-terraform/main.tf
+++ projekt-04-hello-terraform/main.tf
@@ -30,3 +30,107 @@ resource "azurerm_virtual_network" "lab" {
   location            = azurerm_resource_group.lab.location
   address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "lab" {
+  name                 = "snet-lab"
+  resource_group_name  = azurerm_resource_group.lab.name
+  address_prefixes     = ["10.0.1.0/24"]
+  virtual_network_name = azurerm_virtual_network.lab.name
+}
+
+resource "azurerm_public_ip" "lab" {
+  name                = "pip-lab"
+  allocation_method   = "Static"
+  resource_group_name = azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+}
+
+resource "azurerm_network_interface" "lab" {
+  name                = "nic-lab"
+  resource_group_name = azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.lab.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.lab.id
+  }
+}
+
+resource "azurerm_network_security_group" "lab" {
+  name                = "nsg-lab"
+  resource_group_name = azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+
+}
+
+resource "azurerm_network_security_rule" "ssh" {
+  name                       = "ssh-lab"
+  priority                   = 1000
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "22"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+
+  resource_group_name         = azurerm_resource_group.lab.name
+  network_security_group_name = azurerm_network_security_group.lab.name
+}
+
+resource "azurerm_network_security_rule" "allowhttp" {
+  name                       = "http-lab"
+  priority                   = 1010
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "80"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+
+  resource_group_name         = azurerm_resource_group.lab.name
+  network_security_group_name = azurerm_network_security_group.lab.name
+}
+
+resource "azurerm_network_interface_security_group_association" "lab" {
+  network_interface_id      = azurerm_network_interface.lab.id
+  network_security_group_id = azurerm_network_security_group.lab.id
+}
+
+resource "azurerm_linux_virtual_machine" "lab" {
+  name                = "vm-lab-marcotf"
+  resource_group_name = azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+  size                = "Standard_D2s_v6"
+  admin_username      = "azureuser"
+
+  custom_data = filebase64("cloud-init.yaml")
+
+  network_interface_ids = [
+    azurerm_network_interface.lab.id
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_ed25519.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.lab.ip_address
+}
