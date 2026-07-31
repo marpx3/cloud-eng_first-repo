04b6284c3823b696b2bada10fa4b2e04fa224d96
Marco Podiwin <marco.podiwin+clouddev@gmail.com>
2026-07-31 14:36:15 +0200

    location und rgname fixed

--- selbsttest-01/modules/basis/main.tf
+++ selbsttest-01/modules/basis/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "this" {
 
 resource "azurerm_storage_account" "this" {
   name                          = var.sa_name
-  resource_group_name           = var.rg_name
-  location                      = var.location
+  resource_group_name           = azurerm_resource_group.this.name
+  location                      = azurerm_resource_group.this.location
   account_tier                  = var.account_tier
   account_replication_type      = var.account_replication_type
   public_network_access_enabled = var.public_network_access_enabled
